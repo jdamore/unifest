a4b2ef51873f8a22246434a9a18bdb6e167ffdb5
Jean D'Amore <jdamore@thoughtworks.com>
2014-10-22 00:17:00 +1000

    Made the UniFest site look a bit better

--- ec2-new.sh
+++ ec2-new.sh
@@ -4,6 +4,6 @@ curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $curr_dir/ec2.config
 
 # Create a new resource
-pushd config  > /dev/null
+pushd config > /dev/null
 ansible-playbook -i local ec2.yml
 popd > /dev/null
